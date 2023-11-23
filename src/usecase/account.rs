c5c9a12a9a6c8af5f93e6023a7cd42f1ab556be6
suba <84484832+suba327777@users.noreply.github.com>
2023-11-23 21:35:18 +0900

    fix: Result type

--- src/usecase/account.rs
+++ src/usecase/account.rs
@@ -2,6 +2,6 @@ use crate::domain::object::account::Account;
 use crate::domain::repository::account::AccountRepository;
 use anyhow::Result;
 
-pub fn post_account(repository: impl AccountRepository, account: Account) -> Result {
+pub fn post_account(repository: impl AccountRepository, account: Account) -> Result<()> {
     repository.insert(account)
 }
