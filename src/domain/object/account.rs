eb6787eaadb8952ec2cdb59b65078b01d31d5266
suba <84484832+suba327777@users.noreply.github.com>
2023-11-23 22:43:35 +0900

    fix: card_id type

--- src/domain/object/account.rs
+++ src/domain/object/account.rs
@@ -1,20 +1,20 @@
 pub struct Account {
     pub id: u64,
     pub username: String,
-    pub card_id: u64,
+    pub card_id: Vec<u8>,
 }
 
 impl Account {
-    pub fn create(username: String, card_id: u64) -> Self {
+    pub fn create(username: String, card_id: Vec<u8>) -> Self {
         Self {
             id: Default::default(),
             username,
             card_id,
         }
     }
-    pub fn register_card_id() -> u64 {
+    pub fn register_card_id() -> Vec<u8> {
         //TODO
-        let card_id = 120;
+        let card_id: Vec<u8> = vec![1, 16, 3, 16, 197, 20, 106, 38];
         return card_id;
     }
 }
