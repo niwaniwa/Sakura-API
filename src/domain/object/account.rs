06cb8fedafc88b6f4a210f937180ecf9b43d1514
suba <84484832+suba327777@users.noreply.github.com>
2023-11-23 20:51:24 +0900

    add: register_card_id fn //TODO

--- src/domain/object/account.rs
+++ src/domain/object/account.rs
@@ -1,15 +1,20 @@
 pub struct Account {
     pub id: u64,
     pub username: String,
-    pub card_id: u8,
+    pub card_id: u64,
 }
 
 impl Account {
-    pub fn create(username: String) -> Self {
+    pub fn create(username: String, card_id: u64) -> Self {
         Self {
             id: Default::default(),
             username,
-            card_id: Default::default(),
+            card_id,
         }
+    }
+    pub fn register_card_id() -> u8 {
+        //TODO
+        let card_id = 120;
+        return card_id;
     }
 }
