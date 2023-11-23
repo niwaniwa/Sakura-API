4973264b42e059129b8afeeeee6bb91fe8ca945e
suba <84484832+suba327777@users.noreply.github.com>
2023-11-23 19:21:12 +0900

    add: post handler

---
+++ src/server/handler/account.rs
@@ -0,0 +1,15 @@
+use super::super::request::account::AccountRequest;
+use crate::server::RequestContext;
+use crate::usecase;
+use actix_web::{post, web, web::Json, HttpResponse, Responder};
+
+#[post("/accounts")]
+async fn post_account(
+    data: web::Data<RequestContext>,
+    request: Json<AccountRequest>,
+) -> impl Responder {
+    match usecase::account::post_account(data.account_repository(), request.of()) {
+        Ok(_) => HttpResponse::NoContent().finish(),
+        Err(_) => HttpResponse::InternalServerError().json(""),
+    }
+}
