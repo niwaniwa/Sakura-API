f2920d059dc32a9b22f4f1003ab2d7aa80589d15
suba <84484832+suba327777@users.noreply.github.com>
2023-11-24 17:20:03 +0900

    remove: println

--- src/server/handler/account.rs
+++ src/server/handler/account.rs
@@ -9,7 +9,6 @@ async fn post_account(
     data: web::Data<RequestContext>,
     request: Json<AccountRequest>,
 ) -> impl Responder {
-    println!("a");
     match usecase::account::post_account(&mut data.account_repository(), &request.of()) {
         Ok(_) => HttpResponse::NoContent().finish(),
         Err(_) => HttpResponse::InternalServerError().json(""),
