8c010c2e5118e1abc867b915712864c59e1d9dfc
suba <84484832+suba327777@users.noreply.github.com>
2023-11-24 13:05:13 +0900

    test: post_account

--- src/usecase/account.rs
+++ src/usecase/account.rs
@@ -2,6 +2,48 @@ use crate::domain::object::account::Account;
 use crate::domain::repository::account::AccountRepository;
 use anyhow::Result;
 
-pub fn post_account(repository: impl AccountRepository, account: Account) -> Result<()> {
+pub fn post_account(repository: &mut impl AccountRepository, account: &Account) -> Result<()> {
     repository.insert(account)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::cell::RefCell;
+    use std::collections::HashMap;
+
+    use anyhow::Result;
+
+    struct MockAccountRepository {
+        pool: RefCell<HashMap<u64, Account>>,
+    }
+
+    impl AccountRepository for MockAccountRepository {
+        fn insert(&self, account: &Account) -> Result<()> {
+            let _ = &self
+                .pool
+                .borrow_mut()
+                .entry(1)
+                .or_insert_with(|| account.clone());
+
+            Ok(())
+        }
+    }
+
+    #[test]
+    fn success_post_account() {
+        let mut repository = MockAccountRepository {
+            pool: RefCell::new(HashMap::new()),
+        };
+
+        let test_account = Account {
+            id: 1,
+            username: "test_user".to_string(),
+            card_id: vec![1, 16, 3, 16, 197, 20, 106, 38],
+        };
+
+        let result = post_account(&mut repository, &test_account);
+        println!("{:?}", result);
+        assert!(result.is_ok());
+    }
 }
