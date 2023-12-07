9680ba233fabb6980ce6ca2ed6ed5b7f093bcb7e
suba <84484832+suba327777@users.noreply.github.com>
2023-12-07 09:08:12 +0900

    add: grade,card_type column

--- migrations/2023-11-22-075134_create_account/up.sql
+++ migrations/2023-11-22-075134_create_account/up.sql
@@ -2,6 +2,8 @@
 CREATE TABLE account (
   id bigserial NOT NULL PRIMARY KEY,
   username varchar(255) NOT NULL UNIQUE,
+  grade INTEGER CHECK (grade >= 1 AND grade <= 4) NOT NULL,
+  card_type varchar(255) NOT NULL,
   card_id bytea NOT NULL,
   created_at TIMESTAMPTZ NOT NULL
 );
