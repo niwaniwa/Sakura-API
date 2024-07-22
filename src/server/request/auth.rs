2a0019a3c523c2082035d6d76500f98dd818f314
suba <84484832+suba327777@users.noreply.github.com>
2024-07-23 08:48:47 +0900

    add: AuthAccountRequest

--- src/server/request/auth.rs
+++ src/server/request/auth.rs
@@ -1,7 +1,17 @@
+use chrono::NaiveDateTime;
 use serde::Deserialize;
 
 #[derive(Debug, Default, Deserialize)]
 pub struct AuthRequest {
+    pub email: String,
+    pub password: String,
+}
+
+#[derive(Debug, Default, Deserialize)]
+pub struct AuthAccountRequest {
+    pub username: String,
+    pub grade: i32,
+    pub expiration_date: NaiveDateTime,
     pub email: String,
     pub password: String,
 }
