f930daa15e345b37ee1d7e72a9444a7a614f5fe9
suba <84484832+suba327777@users.noreply.github.com>
2024-07-23 15:49:44 +0900

    add: redis connection

--- src/server/connection.rs
+++ src/server/connection.rs
@@ -1,7 +1,9 @@
 use std::env;
 
 use diesel::prelude::*;
-use diesel::r2d2::{ConnectionManager, Pool};
+use diesel::r2d2::ConnectionManager;
+use r2d2_redis::RedisConnectionManager;
+
 use dotenv::dotenv;
 
 use crate::domain::repository::account::AccountRepository;
@@ -16,35 +18,45 @@ use crate::infrastructures::repository::door::DoorRepositoryImpl;
 use crate::infrastructures::repository::register::RegisterRepositoryImpl;
 
 pub struct RequestContext {
-    pool: Pool<ConnectionManager<PgConnection>>,
+    db_pool: diesel::r2d2::Pool<ConnectionManager<PgConnection>>,
+    redis_pool: r2d2_redis::r2d2::Pool<RedisConnectionManager>,
 }
 
 impl RequestContext {
     pub fn new() -> RequestContext {
         dotenv().ok();
         let db_url = env::var("DATABASE_URL").expect("DATABASE_URL i not set");
-        let manager = ConnectionManager::<PgConnection>::new(db_url);
-        let pool = Pool::builder()
-            .build(manager)
+        let db_manager = ConnectionManager::<PgConnection>::new(db_url);
+        let db_pool = diesel::r2d2::Pool::builder()
+            .build(db_manager)
             .expect("Failed to create DB connection pool.");
 
-        RequestContext { pool }
+        let redis_url = env::var("REDIS_URL").expect("REDIS_URL is not set");
+        let redis_manager = RedisConnectionManager::new(redis_url).unwrap();
+        let redis_pool = r2d2_redis::r2d2::Pool::builder()
+            .build(redis_manager)
+            .expect("Failed to create Redis connection pool.");
+
+        RequestContext {
+            db_pool,
+            redis_pool,
+        }
     }
 
     pub fn account_repository(&self) -> impl AccountRepository {
         AccountRepositoryImpl {
-            pool: Box::new(self.pool.to_owned()),
+            pool: Box::new(self.db_pool.to_owned()),
         }
     }
     pub fn card_repository(&self) -> impl CardRepository {
         CardRepositoryImpl {
-            pool: Box::new(self.pool.to_owned()),
+            pool: Box::new(self.db_pool.to_owned()),
         }
     }
 
     pub fn door_repository(&self) -> impl DoorRepository {
         DoorRepositoryImpl {
-            pool: Box::new(self.pool.to_owned()),
+            pool: Box::new(self.db_pool.to_owned()),
         }
     }
 
@@ -54,7 +66,7 @@ impl RequestContext {
 
     pub fn auth_repository(&self) -> impl AuthRepository {
         AuthRepositoryImpl {
-            pool: Box::new(self.pool.to_owned()),
+            pool: Box::new(self.db_pool.to_owned()),
         }
     }
 }
